cf5e8ef8dbe3af47eac81b954900adf3bd7a77b8
Garam Cha <icky7080@naver.com>
2021-09-15 22:28:41 +0900

    2021년 09월 08일

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common import keys
+from selenium.webdriver.common import action_chains
 from selenium.webdriver.common.keys import Keys
-import requests
 from bs4 import BeautifulSoup
+from selenium.webdriver.common.action_chains import ActionChains
 
 # cmd cd C:\Program Files\Google\Chrome\Application
 # cmd chrome.exe --remote-debugging-port=9222 --user-data-dir="C:/ChromeTemp"
+
+# variable
+device_var = 5
+port_var = 5
+countries_var = "Angola"
 
 # chrome launch
 chrome_options = Options()
@@ -19,41 +25,44 @@ driver = webdriver.Chrome(
 driver.implicitly_wait(10)
 
 # countries dropdown click
-driver.find_element_by_css_selector(
-    '.statusBtn.btn.dropdown-toggle.btn-primary').click()
-time.sleep(2)
+elem = driver.find_element_by_css_selector(
+    '.statusBtn.btn.dropdown-toggle.btn-primary')
+ActionChains(driver).click(elem).perform()
 
 # countries search click and sendkeys
-countries_var = "Andorra"
-driver.find_element_by_css_selector('.form-control.countriesSearch').clear()
-driver.find_element_by_css_selector(
-    '.form-control.countriesSearch').send_keys(countries_var)
-time.sleep(2)
+elem = driver.find_element_by_css_selector('.form-control.countriesSearch')
+elem.clear()
+ActionChains(driver).click(elem).send_keys(countries_var).perform()
+time.sleep(1)
 
 # countries choose click
-driver.find_element_by_css_selector('.countryChoose').click()
-time.sleep(2)
+elem = driver.find_element_by_css_selector('.countryChoose')
+ActionChains(driver).click(elem).perform()
 
 # service search click and sendkeys
 service_var = "Kakao"
-driver.find_element_by_css_selector('#servicesSearch').clear()
-driver.find_element_by_css_selector('#servicesSearch').send_keys(service_var)
-time.sleep(5)
+elem = driver.find_element_by_id('servicesSearch')
+elem.clear()
+ActionChains(driver).send_keys_to_element(elem, service_var).perform()
+time.sleep(2)
 
 # kakaotalk service click
-driver.find_element_by_css_selector('#leftForm').click()
-driver.find_element_by_css_selector(
-    '.table.table-hover.table-stripped.table-condensed.table-small.tabbed').click()
+elem = driver.find_element_by_css_selector(
+    '.table.table-hover.table-stripped.table-condensed.table-small.tabbed')
+ActionChains(driver).click(elem).perform()
+time.sleep(0.5)
 
-# kakaotalk service set and buy
-device_var = 2
-driver.find_element_by_css_selector('.form-control.numbersToBuy').click()
-driver.find_element_by_css_selector(
-    '.form-control.numbersToBuy').send_keys(Keys.BACKSPACE)
-driver.find_element_by_css_selector(
-    '.form-control.numbersToBuy').send_keys(device_var)
-driver.find_element_by_css_selector(
-    '.table.table-hover.table-stripped.table-condensed.table-small.tabbed').click()
+# kakaotalk service set
+elem = driver.find_element_by_css_selector('.form-control.numbersToBuy')
+elem.clear()
+ActionChains(driver).click(elem).send_keys(device_var).perform()
+time.sleep(0.5)
+
+# kakaotalk service buy
+elem = driver.find_element_by_css_selector(
+    '#leftForm > table > tbody > tr.tabbed.cell.info > td.radiotd > label > div > a')
+ActionChains(driver).move_to_element(elem).click().perform()
+time.sleep(15)
 
 # html scarping
 soup = BeautifulSoup(driver.page_source, "lxml")
@@ -73,65 +82,64 @@ rows = soup.find("table", {"id": "getNumberTable"}).find_all("strong")
 for row in rows:
     sms_list.append(row.get_text())
 
-# chrome tabs switch
-driver.switch_to_window(driver.window_handles[1])
-# driver.switch_to_window(driver.window_handles[0])
-time.sleep(0.5)
+# selenium page switch
+driver.get("https://dashboard.soax.com/proxy")
 
 # ip address click
-driver.find_element_by_css_selector(
-    'body > div.page__wrapper.page__wrapper--with-subbar.packages > div.page__main-content.package > div.package__sect-twice > section:nth-child(1) > div.panel > div > div.sx-tabs__header > div:nth-child(2)').click()
-time.sleep(1)
+elem = driver.find_element_by_css_selector(
+    'body > div.page__wrapper.page__wrapper--with-subbar.packages > div.page__main-content.package > div.package__sect-twice > section:nth-child(1) > div.panel > div > div.sx-tabs__header > div:nth-child(2)')
+ActionChains(driver).click(elem).perform()
+time.sleep(0.5)
+
+# add filter click
+elem = driver.find_element_by_css_selector('.ip-port__add-new-text')
+ActionChains(driver).click(elem).perform()
+time.sleep(0.5)
 
 # country click
-driver.find_element_by_css_selector(
-    'body > div.page__wrapper.page__wrapper--with-subbar.packages > div.page__main-content.package > div.package__sect-twice > section:nth-child(1) > div.panel > div > div.sx-tabs__body > div.sx-tabs__panel.ip-address-tab.is-active > form > div:nth-child(1) > div.sx-form__row-content > div > button').click()
-time.sleep(1)
+elem = driver.find_element_by_css_selector('body > div.page__wrapper.page__wrapper--with-subbar.packages > div.page__main-content.package > div.package__sect-twice > section:nth-child(1) > div.panel > div > div.sx-tabs__body > div.sx-tabs__panel.ip-address-tab.is-active > form > div.content-row.sx-form__row.add-filter-container > div.sx-form__row-content.pckg-ip-port__filter-wrap > div.content-mwrap.flex.flex--wrap.targets-container.ip-port-targets > div:nth-child(2) > div > div > div')
+ActionChains(driver).click(elem).perform()
 
 # country search click and sendkeys
-driver.find_element_by_css_selector('body > div.page__wrapper.page__wrapper--with-subbar.packages > div.page__main-content.package > div.package__sect-twice > section:nth-child(1) > div.panel > div > div.sx-tabs__body > div.sx-tabs__panel.ip-address-tab.is-active > form > div.content-row.sx-form__row.add-filter-container > div.sx-form__row-content.pckg-ip-port__filter-wrap > div.content-mwrap.flex.flex--wrap.targets-container.ip-port-targets > div:nth-child(2) > div > div > div').click()
-time.sleep(1)
-driver.find_element_by_css_selector(
-    'div.tooltip__inner > div.sx-select__search > input').clear()
-driver.find_element_by_css_selector(
-    'div.tooltip__inner > div.sx-select__search > input').send_keys(countries_var)
-driver.find_element_by_css_selector(
-    'div.tooltip__inner > div.sx-select__list.ps').click()
+elem = driver.find_element_by_css_selector(
+    'div.tooltip__inner > div.sx-select__search > input')
+ActionChains(driver).move_to_element(elem).send_keys(countries_var).perform()
+time.sleep(2)
 
-# number of ports set
-port_var = 3
-driver.find_element_by_css_selector('body > div.page__wrapper.page__wrapper--with-subbar.packages > div.page__main-content.package > div.package__sect-twice > section:nth-child(1) > div.panel > div > div.sx-tabs__body > div.sx-tabs__panel.ip-address-tab.is-active > form > div.content-row.sx-form__row.add-filter-container > div.sx-form__row-content.pckg-ip-port__filter-wrap > div.lbl-param-list > div > div > input').clear()
-driver.find_element_by_css_selector('body > div.page__wrapper.page__wrapper--with-subbar.packages > div.page__main-content.package > div.package__sect-twice > section:nth-child(1) > div.panel > div > div.sx-tabs__body > div.sx-tabs__panel.ip-address-tab.is-active > form > div.content-row.sx-form__row.add-filter-container > div.sx-form__row-content.pckg-ip-port__filter-wrap > div.lbl-param-list > div > div > input').send_keys(port_var)
+# searched country click
+elem = driver.find_element_by_css_selector(
+    'div.tooltip__inner > div.sx-select__list.ps')
+ActionChains(driver).click(elem).perform()
+
+# number of port set
+elem = driver.find_element_by_css_selector(
+    'body > div.page__wrapper.page__wrapper--with-subbar.packages > div.page__main-content.package > div.package__sect-twice > section:nth-child(1) > div.panel > div > div.sx-tabs__body > div.sx-tabs__panel.ip-address-tab.is-active > form > div.content-row.sx-form__row.add-filter-container > div.sx-form__row-content.pckg-ip-port__filter-wrap > div.lbl-param-list > div > div > input')
+elem.clear()
+ActionChains(driver).send_keys_to_element(elem, port_var).perform()
 
 # save button click
-driver.find_element_by_css_selector('body > div.page__wrapper.page__wrapper--with-subbar.packages > div.page__main-content.package > div.package__sect-twice > section:nth-child(1) > div.panel > div > div.sx-tabs__body > div.sx-tabs__panel.ip-address-tab.is-active > form > div.content-row.sx-form__row.add-filter-container > div.sx-form__row-content.pckg-ip-port__filter-wrap > div.flex.flex--justify.flex--wrap.content-mwrap > button.button.button--04.package__form-btn.margin--t.add-filter-save').click()
+elem = driver.find_element_by_css_selector('body > div.page__wrapper.page__wrapper--with-subbar.packages > div.page__main-content.package > div.package__sect-twice > section:nth-child(1) > div.panel > div > div.sx-tabs__body > div.sx-tabs__panel.ip-address-tab.is-active > form > div.content-row.sx-form__row.add-filter-container > div.sx-form__row-content.pckg-ip-port__filter-wrap > div.flex.flex--justify.flex--wrap.content-mwrap > button.button.button--04.package__form-btn.margin--t.add-filter-save')
+ActionChains(driver).click(elem).perform()
+time.sleep(2)
 
-# scraping port list
+# scraping country, port list
 soup = BeautifulSoup(driver.page_source, "lxml")
 
-# scraping and port list append
+country_port_list = []
 rows = soup.find_all("div", {"class": "sx-form__item ip-port"})
-port_list = []
-port_index = 0
 
 for row in rows:
 
-    country_rows = row.find_all("div", {"class": "ip-port__country"})
+    country = row.find("div", {"class": "ip-port__country"}).get_text().strip()
 
-    for country_row in country_rows:
+    port = row.find("div", {"class": "ip-port__info"}).get_text().strip()
 
-        if (countries_var == country_row.get_text().strip()):
-            print("같음")
-            break
-        print("반복중")
-        port_index += 1
+    if (countries_var == country):
 
-    port_rows = row.find_all("div", {"class": "ip-port__info"})
+        country_port_list.append(country)
 
-    for port_row in port_rows:
+        country_port_list.append(port.replace(" ", "").replace("-", ""))
 
-        port_list.append(port_row.get_text().strip())
-
-print(port_index)
-
-print(port_list[port_index])
+# make minimum port, maximum port and country_port_list append
+minimum_port = country_port_list[1][:5]
+maximum_port = country_port_list[1][5:]
